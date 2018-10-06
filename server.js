70249e850c204fe2f4e08de47c6d8265a774d677
shinkarenko <shammy@list.ru>
2018-10-06 17:51:39 +0300

    new version server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const http = require ("http");
-let server =
-    http.createServer ((req, res) => {
-        res.statusCode=200;
-        res.setHeader('Content-type','text/plain');
-        res.end('Hello world\n');
-    });
+let server = new http.Server();
 server.listen(3000,'127.0.0.1',()=>{
     console.log("Server running at ", "http://127.0.0.1:3000/");
+});
+server.on("request", (req, res) =>{
+   res.end("Hello world\n");
 });
 
 module.exports.server = server
