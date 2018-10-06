8999adf97579dd408bf24148fb02f8976919cd6c
shinkarenko <shammy@list.ru>
2018-10-06 18:35:25 +0300

    work version emitter.js

--- emitter.js
+++ emitter.js
@@ -2,10 +2,11 @@ const EventEmitter = require ("events").EventEmitter;
 let manager = new EventEmitter();
 manager.on("request", (request) =>{
     request.data="data";
-    manager.response = request.data;
+    manager.response = request;//.data;
+   // console.log(manager.response);
 });
-//manager.on("request", (request)=>{
-//    manager.response=request.data;
-//});
+
+
+//manager.emit("request", {myData:"Моя дата"});
 
 module.exports.manager = manager
